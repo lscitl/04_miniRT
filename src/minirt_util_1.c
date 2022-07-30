2b96f5f035dc25ca5cb1830994081b1c5f95e701
Seungduk Seo <seseo@student.42seoul.kr>
2022-07-31 00:27:37 +0900

    color funtions added.

--- src/minirt_util_1.c
+++ src/minirt_util_1.c
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 19:58:09 by seseo             #+#    #+#             */
-/*   Updated: 2022/07/27 14:56:35 by seseo            ###   ########.fr       */
+/*   Updated: 2022/07/30 22:11:59 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,42 @@
 int	get_color(int color, int target_color)
 {
 	return ((color >> target_color * 8) & 0xFF);
+}
+
+t_color	set_color(double red, double green, double blue)
+{
+	t_color	color;
+
+	color.r = red;
+	color.g = green;
+	color.b = blue;
+	return (color);
+}
+
+t_color	add_color(t_color c1, t_color c2, t_color c3)
+{
+	t_color	color;
+
+	color.r = c1.r + c2.r + c3.r;
+	color.g = c1.g + c2.g + c3.g;
+	color.b = c1.b + c2.b + c3.b;
+	// if (color.r > 1)
+	// 	color.r = 1;
+	// if (color.g > 1)
+	// 	color.g = 1;
+	// if (color.b > 1)
+	// 	color.b = 1;
+	return (color);
+}
+
+t_color	apply_bright(t_color light, double bright)
+{
+	t_color	color;
+
+	color.r = light.r * bright;
+	color.g = light.g * bright;
+	color.b = light.b * bright;
+	return (color);
 }
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
